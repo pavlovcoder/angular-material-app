783834564567d560c7e7cd2d3060be843855e12a
Vladimir <pavlovcoder@gmail.com>
2018-09-02 21:13:03 +0200

    Commit #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-material-app';
+  public title: string = 'angular-material-app';
+  public myControl: any = new FormControl();
+  public options: string[] = [
+    'iPhone X',
+    'iPhone 10',
+    'iPhone 8',
+    'iPhone 8 Plus',
+    'iPhone 7',
+    'iPhone 7 Plus',
+    'iPhone 6s',
+    'iPhone 6s Plus'
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule } from '@angular/material';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,13 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
